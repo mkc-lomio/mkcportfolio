460f0cc6164828c6ee9bfba278663c135319844e
mkc-lomio <marckenneth.lomio@gmail.com>
2026-02-12 10:43:49 +0800

    fix deployment build

--- client/types/ionicons.d.ts
+++ client/types/ionicons.d.ts
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 
-declare namespace JSX { 
-  interface IntrinsicElements {
-    "ion-icon": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement> & { name?: string },
-      HTMLElement
-    >;
+import "react";
+
+declare module "react" {
+  namespace JSX {
+    interface IntrinsicElements {
+      "ion-icon": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & { name?: string },
+        HTMLElement
+      >;
+    }
   }
 }

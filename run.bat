532ee53f9c9008ed2332d5684d73b9fa0cafcce0
mkc-lomio <marckenneth.lomio@gmail.com>
2026-02-23 23:41:38 +0800

    run.bat

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ REM Open browser after a short delay (gives servers time to start)
 start "" cmd /c "timeout /t 3 /noq >nul && start http://localhost:3000"
 
 REM Run Next.js (client folder)
-start "" cmd /k "cd /d %~dp0client && npm run dev"
+start "" cmd /k "cd /d %~dp0client && npx next dev --webpack"
 
 REM Run Express.js (server folder)
-start "" cmd /k "cd /d %~dp0server && npm run start"
+REM start "" cmd /k "cd /d %~dp0server && npm run start"
